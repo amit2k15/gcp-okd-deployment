f1486d4a39409c5718e5dfe9e2dd8db74dc3cfd5
amit2k15 <69316540+amit2k15@users.noreply.github.com>
2025-06-27 11:32:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,27 @@
-resource "google_compute_instance" "k8s_vm" {
-  name         = "k8s-vm"
-  machine_type = "custom-2-4096"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+  credentials = var.gcp_credentials
+}
+
+resource "google_compute_instance" "jenkins_vm" {
+  name         = "jenkins-vm"
+  machine_type = "e2-medium"  # Recommended minimum for Jenkins
   zone         = var.gcp_zone
 
   boot_disk {
     initialize_params {
-      image = "centos-cloud/centos-stream-9"
-      size  = 100
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"  # Common choice for Jenkins
+      size  = 50  # GB (can be adjusted based on needs)
       type  = "pd-ssd"
     }
   }
@@ -20,18 +35,25 @@ resource "google_compute_instance" "k8s_vm" {
     ssh-keys = "${var.ssh_user}:${var.ssh_pub_key}"
   }
 
-  tags = ["jenkins-server"]
+  # Uncomment and modify if you have a Jenkins installation script
+  # metadata_startup_script = file("${path.module}/scripts/install_jenkins.sh")
+
+  tags = ["jenkins-server", "http-server"]
 }
 
-resource "google_compute_firewall" "allow_jenkins" {
-  name    = "allow-jenkins-traffic"
+resource "google_compute_firewall" "allow_jenkins_ports" {
+  name    = "allow-jenkins-ports"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443", "8080", "6443", "8443", "3306", "30000-32767", "3000", "10051", "10050"]
+    ports    = ["8080", "50000", "80", "443"]  # Standard Jenkins ports + web ports
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["jenkins-server"]
+  target_tags   = ["jenkins-server", "http-server"]
+}
+
+output "vm_public_ip" {
+  value = google_compute_instance.jenkins_vm.network_interface[0].access_config[0].nat_ip
 }
