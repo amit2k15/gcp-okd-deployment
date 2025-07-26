3782fc1d92635db8f61bbecf2a9b154de35cb46c
amit2k15 <69316540+amit2k15@users.noreply.github.com>
2025-07-26 13:25:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
   description = "Public IP address of the VM instance"
-  value       = google_compute_instance.jenkins_vm.network_interface[0].access_config[0].nat_ip
+  value       = google_compute_instance.zabbix7.4_vm.network_interface[0].access_config[0].nat_ip
 }
 
 output "ssh_command" {
   description = "Command to SSH into the instance"
-  value       = "ssh ${var.ssh_user}@${google_compute_instance.jenkins_vm.network_interface[0].access_config[0].nat_ip}"
+  value       = "ssh ${var.ssh_user}@${google_compute_instance.zabbix7.4_vm.network_interface[0].access_config[0].nat_ip}"
 }
